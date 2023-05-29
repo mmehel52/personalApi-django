fe0a9176e00ad7627e18b47f587b4260911741f3
mustafa mehel <mehelx5207@gmail.com>
2023-05-29 22:02:33 +0300

    views

--- personel/views.py
+++ personel/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .serializers import DepartmentSerializer,PersonelSerializer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from .models import Department,Personel
 
-# Create your views here.
+class DepartmentListCreateView(ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class DepartmentRUDView(RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class PersonnelListCreateView(ListCreateAPIView):
+    queryset = Personel.objects.all()
+    serializer_class = PersonelSerializer
+
+
+class PersonnelRUDView(RetrieveUpdateDestroyAPIView):
+    queryset = Personel.objects.all()
+    serializer_class = PersonelSerializer
