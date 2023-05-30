faaf412e5f8b3f444c85c206c635fcdf57899bfe
mustafa mehel <mehelx5207@gmail.com>
2023-05-30 20:34:55 +0300

    ghg

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'personel',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- personel/views.py
+++ personel/views.py
@@ -2,15 +2,47 @@ from django.shortcuts import render
 from .serializers import (DepartmentSerializer,PersonelSerializer,DepartmentPersonelSerializer)
 from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView,ListAPIView)
 from .models import Department,Personel
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class DepartmentListCreateView(ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes=(
+        IsAuthenticated,
+        IsAdminOrReadOnly
+    )
 
 
 class DepartmentRUDView(RetrieveUpdateDestroyAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes=[IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        if self.request.user.is_superuser or self.request.user.ıs_staff:
+            return self.update(request,*args,**kwargs)
+        data={
+            'message':'you are not authorized to update!'
+        }
+        return Response(data,status=status.HTTP_401_UNAUTHORIZED)
+
+    def destroy(self, request, *args, **kwargs):
+
+        instance = self.get_object()
+
+        if self.request.user.is_superuser:
+            #self.perform_destroy(instance)
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        data={
+            'message':'you are not authorized to delete!'
+        }
+        return Response(data,status=status.HTTP_204_NO_CONTENT)
+
 
 
 class PersonelListCreateView(ListCreateAPIView):
@@ -21,6 +53,29 @@ class PersonelListCreateView(ListCreateAPIView):
 class PersonelRUDView(RetrieveUpdateDestroyAPIView):
     queryset = Personel.objects.all()
     serializer_class = PersonelSerializer
+    permission_classes=[IsAuthenticated]
+
+    def put(self,request,**args,**kwargs):
+        if self.request.user.is_superuser or self.request.user.ıs_staff:
+            return self.update(request,*args,**kwargs)
+        data={
+            'message':'you are not authorized to update!'
+        }
+        return Response(data,status=status.HTTP_401_UNAUTHORIZED)
+
+    def destroy(self, request, *args, **kwargs):
+
+        instance = self.get_object()
+
+        if self.request.user.is_superuser:
+            #self.perform_destroy(instance)
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        data={
+            'message':'you are not authorized to delete!'
+        }
+        return Response(data,status=status.HTTP_204_NO_CONTENT)
+
 
 class DepartmentPersonelView(ListAPIView):
     queryset=Department.objects.all()
