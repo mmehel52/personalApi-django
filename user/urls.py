960cf335edfef6bac8c476bb3b0388dfa9894fec
mustafa mehel <mehelx5207@gmail.com>
2023-05-31 15:09:13 +0300

    profile

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import RegisterView
+from .views import RegisterView,ProfileUpdateView
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('register/',RegisterView.as_view()),
+    path('profile/',ProfileUpdateView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView,RetrieveUpdateAPIView
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,ProfileSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from .models import Profile
 
 class RegisterView(CreateAPIView):
     queryset=User.objects.all()
@@ -24,3 +25,7 @@ class RegisterView(CreateAPIView):
 
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+    
+class ProfileUpdateView(RetrieveUpdateAPIView):
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerializer
